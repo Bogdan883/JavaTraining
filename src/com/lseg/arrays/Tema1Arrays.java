e5f29bba9be9e1bf0736260218be48d65c8e9c82
Bogdan883 <bogdan.dima@gmail.com>
2020-08-19 23:01:50 +0300

    First commit

---
+++ src/com/lseg/arrays/Tema1Arrays.java
@@ -0,0 +1,35 @@
+package com.lseg.arrays;
+
+public class Tema1Arrays {
+
+    public static void main(String[] args)
+    {
+        // Creare array initial
+        int n = 7;
+        int d = 2;
+        int[] a = new int[n];
+        System.out.print ("Array-ul initial: ");
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = i+1;
+            System.out.print (a[i]+" ");
+        }
+        System.out.print ("\nNumar rotatii stanga: "+d);
+        // Rotatie la stanga
+
+        for (int i = 0; i < d; i++)
+        {
+            int aux = a[0];
+            for (int j = 0; j < n-1; j++)
+            {
+                a[j] = a[j+1];
+            }
+            a[n-1] = aux;
+        }
+        System.out.print("\nArray-ul final: ");
+        for (int i = 0; i < a.length; i++)
+        {
+            System.out.print (a[i]+" ");
+        }
+    }
+}
