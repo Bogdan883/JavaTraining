87514721b3b57a078256ec0c61e81eab6e4dbe3d
Bogdan883 <bogdan.dima@gmail.com>
2020-10-28 23:11:00 +0200

    Teme

---
+++ src/com/lseg/Tema10/Exercise1.java
@@ -0,0 +1,26 @@
+package com.lseg.Tema10;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        try{
+            try{
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+                System.out.println(x/y);
+            }catch (InputMismatchException error){
+                System.out.println("java.util.InputMismatchException");
+            }
+
+        }catch(Exception error){
+            System.out.println(error);
+        }
+
+    }
+}
